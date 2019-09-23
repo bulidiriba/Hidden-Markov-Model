f75a9803fea8af54025b39ec7f4664ebeb8cc5e0
bulidiriba <bulidiriba@gmail.com>
2019-09-23 18:55:47 +0300

    first commit

---
+++ hmm/hmm_class.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+
+class hmm:
+
+    def __init__(self):
+        pass
+
+    def forward_algorithim(self):
+        pass
+
+    def viterbi_algorithim(self):
+        pass
+
+    def backward_algorithim(self):
+        pass
+
+    def do_estep(self):
+        pass
+
+    def do_mstep(self):
+        pass
+
